7ede06e1aafccba3a2bd2b9ee3837ace2236d586
mithilesh <mithilesht@cloudthat.com>
2023-01-10 17:26:26 +0530

    removed unwanted comments

--- src/Config.js
+++ src/Config.js
@@ -1,10 +1,4 @@
 import { data } from "./data"
-
-// User Pool ID: ap-south-1_aGchBKOL3
-// App Client Id: 29b8n20s0onououhumrj6b6qqr
-// region : ap-south-1
-
-
 //Variables to replace
 const poolId = data.USER_POOL_ID
 const clientId = data.APP_CLIENT_ID
